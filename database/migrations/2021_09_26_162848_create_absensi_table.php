45ea561c89c5baf9c4ed276a471acc6e49336190
Muhamad Prasetyo <muhamad.mprasetyo@gmail.com>
2021-09-27 07:08:18 +0700

    membuat migration absensi

---
+++ database/migrations/2021_09_26_162848_create_absensi_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAbsensiTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('absensi', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',250);
+            $table->string('thumbnail', 250);
+            $table->string('email', 250);
+            $table->boolean('in_room')->default(0);
+            $table->dateTimeTz('shedulce')->nullable();
+            $table->softDeletes('deleted_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('absensi');
+    }
+}
